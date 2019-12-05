310b58695e513abd1556c75edbda9d6a771238d7
Tiago Tescaro <tiago.tescaro@hotmail.com>
2019-12-05 11:33:05 -0300

    ON-13835 - Fix compatible with versions php

--- view/admin/queue/grid.php
+++ view/admin/queue/grid.php
@@ -1,20 +1,6 @@
 <div class="wrap">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-    <?php $this->table() ?>
-    <?/*<form action="<?php echo esc_html(admin_url('admin-post.php')); ?>" method="POST" style="display: inline;">
-        <?php $page =  B2W\SkyHub\View\Admin\Admin::SLUG_QUEUE_INTEGRATION_SKYHUB_LIST;?>
-        <input type="hidden" name="page" value="<?php echo $page;?>"/>
-        <?php wp_referer_field();?>
-        <?php
-        submit_button(
-            __('Clear Queue', \B2W\SkyHub\View\Admin\Admin::DOMAIN),
-            'primary large',
-            'submit',
-            false
-        );
-        ?>
-    </form>*/?>
-    
+    <?php $this->table() ?>    
     <form action="<?php echo esc_html(admin_url('admin-post.php')); ?>" method="POST" style="display: inline;">
         <?php $page =  B2W\SkyHub\View\Admin\Admin::SLUG_QUEUE_INTEGRATION_SKYHUB_EXECUTE;?>
         <input type="hidden" name="page" value="<?php echo $page;?>"/>
