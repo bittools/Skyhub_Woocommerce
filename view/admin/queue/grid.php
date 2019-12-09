eeb98696bb7a85d522a28ce2a212f4fe5f396e75
Tiago Tescaro <tiago.tescaro@hotmail.com>
2019-12-09 15:12:34 -0300

    ON-13853 - Clear queue and confirm

--- view/admin/queue/grid.php
+++ view/admin/queue/grid.php
@@ -1,17 +1,43 @@
 <div class="wrap">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
     <?php $this->table() ?>    
-    <form action="<?php echo esc_html(admin_url('admin-post.php')); ?>" method="POST" style="display: inline;">
+    <form id="formQueue" action="<?php echo esc_html(admin_url('admin-post.php')); ?>" method="POST">
         <?php $page =  B2W\SkyHub\View\Admin\Admin::SLUG_QUEUE_INTEGRATION_SKYHUB_EXECUTE;?>
-        <input type="hidden" name="page" value="<?php echo $page;?>"/>
+        <input type="hidden" id="page" name="page" value="<?php echo $page;?>"/>
         <?php wp_referer_field();?>
-        <?php
-        submit_button(
-            __('Execute Queue', \B2W\SkyHub\View\Admin\Admin::DOMAIN),
-            'primary large',
-            'submit',
-            false
-        );
-        ?>
+        <input 
+            type="button" 
+            class='button button-primary button-large' 
+            onclick="submitQueue(
+                        '<?=__('Do you realy want to execute your queue?', \B2W\SkyHub\View\Admin\Admin::DOMAIN)?>',
+                        '<?=B2W\SkyHub\View\Admin\Admin::SLUG_QUEUE_INTEGRATION_SKYHUB_EXECUTE?>'
+                    );"
+            value="<?=__('Execute Queue', \B2W\SkyHub\View\Admin\Admin::DOMAIN)?>"
+        />
+        <input 
+            type="button" 
+            class='button button-primary button-large' 
+            onclick="submitQueue(
+                        '<?=__('Do you realy want to clear your queue?', \B2W\SkyHub\View\Admin\Admin::DOMAIN)?>',
+                        '<?=B2W\SkyHub\View\Admin\Admin::SLUG_QUEUE_INTEGRATION_SKYHUB_LIST?>'
+                    );"
+            value="<?=__('Clear Queue', \B2W\SkyHub\View\Admin\Admin::DOMAIN)?>"
+        />
     </form>
 </div>
+
+<script>
+    function submitQueue(msg, page)
+    {
+        if (window.confirm(msg))
+        {
+            if (document.getElementById('page')) {
+                document.getElementById('page').value = page;
+            }
+
+            if (document.getElementById('formQueue')) {
+                document.getElementById('formQueue').submit();
+            }
+        }
+    }
+</script>
