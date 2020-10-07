cb969c1484f89d5307c2f86a93a5b7131864c894
Evandro Veloso Gomes <evandro.gomes@e-smart.com.br>
2020-10-07 09:31:20 -0300

    Change version

--- woocommerce-b2w-skyhub.php
+++ woocommerce-b2w-skyhub.php
@@ -4,7 +4,7 @@
  * Plugin Name: B2W WooCommerce
  * Plugin URI:
  * Description: Módulo oficial de integração Woocommerce -> Skyhub
- * Version: 0.2.9
+ * Version: 0.2.10
  * Author: B2W
  * Author URI: https://ri.b2w.digital/
  * Text Domain: woocommerce-b2w-skyhub
