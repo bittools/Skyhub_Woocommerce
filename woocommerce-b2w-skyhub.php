b20f9a0623698091802a3f140e4c4402c3f828bb
Tiago Tescaro <tiago.tescaro@hotmail.com>
2021-11-07 19:03:31 -0300

    feature/change-tracking - add novos campos

--- woocommerce-b2w-skyhub.php
+++ woocommerce-b2w-skyhub.php
@@ -4,7 +4,7 @@
  * Plugin Name: B2W WooCommerce
  * Plugin URI:
  * Description: Módulo oficial de integração Woocommerce -> Skyhub
- * Version: 0.2.18
+ * Version: 0.2.19
  * Author: B2W
  * Author URI: https://ri.b2w.digital/
  * Text Domain: woocommerce-b2w-skyhub
